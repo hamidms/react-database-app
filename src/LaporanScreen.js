9e36e25d07791bc4150c15095843a365bf71a1a1
Hamid Sukardi <hamid@Hamids-MacBook-Pro.local>
2024-01-08 14:14:42 +0700

    adding bottom navigation

---
+++ src/LaporanScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LaporanScreen = () => {
+    return (
+        <View>
+            <Text>Laporan</Text>
+        </View>
+    )
+}
+
+export default LaporanScreen;
